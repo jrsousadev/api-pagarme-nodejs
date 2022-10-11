7594d0ae7f2f70bdd870b32f32c06f27c26f7bf1
Junior Sousa <jrsousadev@gmail.com>
2022-10-11 09:23:04 -0300

    update postback route

--- src/shared/http/routes/postback-routes.ts
+++ src/shared/http/routes/postback-routes.ts
@@ -3,6 +3,6 @@ import PostbackController from "../../controllers/PostbackControllers/PostbackCo
 
 const postbackRoutes = Router();
 
-postbackRoutes.post("/pagarme", PostbackController.pagarme);
+postbackRoutes.post(`/pagarme/:tokenAccess`, PostbackController.pagarme);
 
 export default postbackRoutes;
