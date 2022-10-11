1d35a1877f42047d2bcb3932f28ede84a0247d5d
Junior Sousa <jrsousadev@gmail.com>
2022-10-11 08:39:02 -0300

    (main): create app.ts

---
+++ src/shared/http/app.ts
@@ -0,0 +1,32 @@
+import "reflect-metadata"
+import dotenv from "dotenv";
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import routes from "./routes";
+import "express-async-errors";
+import '../container/';
+
+import { AppError } from "../errors/AppError";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+  if(err instanceof AppError){
+    return response.status(err.statusCode).json({
+      message: err.message
+    });
+  }
+
+  return response.status(500).json({
+    status: "error",
+    message: `Internal server error - ${err.message}`
+  });
+});
+
+export default app;
