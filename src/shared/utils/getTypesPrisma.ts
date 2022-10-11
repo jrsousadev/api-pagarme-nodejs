3d3dee2797f45bb0b6d4f5c8ab7f21a81530b81b
Junior Sousa <jrsousadev@gmail.com>
2022-10-11 08:21:15 -0300

    (main): create utils/getTypesPrisma

---
+++ src/shared/utils/getTypesPrisma.ts
@@ -0,0 +1,30 @@
+import { PaymentType, Status } from "@prisma/client";
+
+export const getPaymentType = (paymentType: string) => {
+  if (paymentType === PaymentType.credit_card) {
+    return PaymentType.credit_card;
+  }
+
+  if (paymentType === PaymentType.pix) {
+    return PaymentType.pix;
+  }
+
+  if (paymentType === PaymentType.billet) {
+    return PaymentType.billet;
+  }
+
+  return "";
+};
+
+export const getStatusType = (status: string) => {
+  if (status === Status.started) return Status.started;
+  if (status === Status.approved) return Status.approved;
+  if (status === Status.chargeback) return Status.chargeback;
+  if (status === Status.error) return Status.error;
+  if (status === Status.pending) return Status.pending;
+  if (status === Status.processing) return Status.processing;
+  if (status === Status.refunded) return Status.refunded;
+  if (status === Status.refused) return Status.refused;
+
+  return "";
+};
