9c96a024499efaf3a0f41d761f1ebd1a9ad9348c
Junior Sousa <jrsousadev@gmail.com>
2022-10-15 15:04:55 -0300

    create router get transaction

--- src/shared/http/routes/transaction-routes.ts
+++ src/shared/http/routes/transaction-routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { expressYupMiddleware } from "express-yup-middleware";
+import { CreateTransactionSchema } from "../schemas/transaction-schemas";
 
 import CreateTransactionController from "../../controllers/TransactionControllers/CreateTransactionController";
-import { CreateTransactionSchema } from "../schemas/transaction-schemas";
+import GetTransactionController from "../../controllers/TransactionControllers/GetTransactionController";
 
 const transactionRoutes = Router();
 
@@ -11,5 +12,7 @@ transactionRoutes.post(
   expressYupMiddleware({ schemaValidator: CreateTransactionSchema }),
   CreateTransactionController.handle
 );
+
+transactionRoutes.get("/", GetTransactionController.handle);
 
 export default transactionRoutes;
