b1101d342cc83fdc793445925745b9243b325603
Junior Sousa <jrsousadev@gmail.com>
2022-10-15 23:43:24 -0300

    update transaction-routes

--- src/shared/http/routes/transaction-routes.ts
+++ src/shared/http/routes/transaction-routes.ts
@@ -1,6 +1,5 @@
 import { Router } from "express";
-import { expressYupMiddleware } from "express-yup-middleware";
-import { CreateTransactionSchema } from "../schemas/transaction-schemas";
+import { validateCreateTransactionSchema } from "../middlewares/validatesSchemas/validateCreateTransactionSchema";
 
 import CreateTransactionController from "../../controllers/TransactionControllers/CreateTransactionController";
 import GetTransactionController from "../../controllers/TransactionControllers/GetTransactionController";
@@ -9,7 +8,7 @@ const transactionRoutes = Router();
 
 transactionRoutes.post(
   "/",
-  expressYupMiddleware({ schemaValidator: CreateTransactionSchema }),
+  validateCreateTransactionSchema,
   CreateTransactionController.handle
 );
 
