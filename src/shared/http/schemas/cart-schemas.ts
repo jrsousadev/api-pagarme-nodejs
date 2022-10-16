6283ba530e899eece89d16bc3c42cef97dd3c154
Junior Sousa <jrsousadev@gmail.com>
2022-10-15 23:43:05 -0300

    update cart-schema

--- src/shared/http/schemas/cart-schemas.ts
+++ src/shared/http/schemas/cart-schemas.ts
@@ -1,28 +1,11 @@
 import * as Yup from "yup";
 
-export const CreateCartSchema = {
-  schema: {
-    body: {
-      yupSchema: Yup.object().shape({
-        code: Yup.string().required(),
-        price: Yup.number().required(),
-      }),
-    },
-  },
-};
+export const CreateCartSchema = Yup.object().shape({
+  code: Yup.string().required(),
+  price: Yup.number().required(),
+});
 
-export const UpdateCartSchema = {
-  schema: {
-    params: {
-      yupSchema: Yup.object().shape({
-        id: Yup.string().required(),
-      }),
-    },
-    body: {
-      yupSchema: Yup.object().shape({
-        code: Yup.string().required(),
-        price: Yup.number().required(),
-      }),
-    },
-  },
-};
+export const UpdateCartSchema = Yup.object().shape({
+  code: Yup.string().required(),
+  price: Yup.number().required(),
+});
