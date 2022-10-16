d5909a4b1607134f7d0a18a4a37d3ba1de4bdd10
Junior Sousa <jrsousadev@gmail.com>
2022-10-16 17:29:50 -0300

    update app use

--- src/shared/http/app.ts
+++ src/shared/http/app.ts
@@ -21,9 +21,9 @@ app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
     if (err instanceof AppError) {
       return response.status(err.statusCode).json({
-      message: err.message
-    });
-  }
+        message: err.message,
+      });
+    }
 
   return response.status(500).json({
     status: "error",
