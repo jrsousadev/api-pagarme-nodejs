f3f657f7a0f03b331da62361d5e97883842739d9
Junior Sousa <jrsousadev@gmail.com>
2022-10-17 20:36:59 -0300

    update module

--- src/shared/useCases/TransactionUseCases/CreateTransactionUseCases.ts
+++ src/shared/useCases/TransactionUseCases/CreateTransactionUseCases.ts
@@ -66,92 +66,99 @@ export class CreateTransactionUseCase {
     billingComplementary,
     pixExpirationDate,
   }: CreateTransactionUseCaseData) => {
-    const cart = await this.cartRepository.readOne({ code: cartCode });
-    if (!cart) throw new AppError("Cart was not found", 400);
+    try {
+      const cart = await this.cartRepository.readOne({ code: cartCode });
+      if (!cart) throw new AppError("Cart was not found", 400);
 
-    let code = uuid();
+      let code = uuid();
 
-    const codeIsExist = await this.transactionRepository.readOne({ code });
-    if (codeIsExist) code = uuid();
+      const codeIsExist = await this.transactionRepository.readOne({ code });
+      if (codeIsExist) code = uuid();
 
-    const initalStatus = "started";
-    const paymentTypeValid = getPaymentType(paymentType);
-    const statusValid = getStatusType(initalStatus);
+      const initalStatus = "started";
+      const paymentTypeValid = getPaymentType(paymentType);
+      const statusValid = getStatusType(initalStatus);
 
-    if (paymentTypeValid === "" || statusValid === undefined) {
-      throw new AppError(`${paymentType} or status invalid`, 400);
+      if (paymentTypeValid === "" || statusValid === undefined) {
+        throw new AppError(`${paymentType} or status invalid`, 400);
+      }
+
+      const infosTransaction: CreateTransactionData = {
+        cartCode: String(cart.id),
+        code,
+        total: cart.price,
+        status: statusValid,
+        paymentType: paymentTypeValid,
+        installments,
+        customerName,
+        customerEmail,
+        customerDocument,
+        customerMobile: parsePhoneNumber(customerMobile, "BR").format("E.164"),
+        billingAddress,
+        billingCity,
+        billingNeighborhood,
+        billingNumber,
+        billingState,
+        billingZipCode,
+        billingComplementary,
+      };
+
+      const transaction = await this.transactionRepository.create(
+        infosTransaction
+      );
+
+      const creditCard = {
+        number: creditCardNumber,
+        holderName: creditCardHolderName,
+        expiration: creditCardExpiration,
+        cvv: creditCardCvv,
+      };
+      const customer = {
+        name: customerName,
+        email: customerEmail,
+        document: customerDocument,
+        mobile: parsePhoneNumber(customerMobile, "BR").format("E.164"),
+      };
+      const billing = {
+        name: "Billing Address",
+        state: billingState,
+        city: billingCity,
+        street: billingAddress,
+        neighborhood: billingNeighborhood,
+        number: billingNumber,
+        zipcode: billingZipCode,
+        complementary: billingComplementary,
+      };
+
+      const responsePagarme = await this.pagarMeProvider.process({
+        creditCard,
+        customer,
+        total: transaction.total,
+        billing,
+        installments,
+        transactionCode: transaction.code,
+        items: [],
+        paymentType,
+        pixExpirationDate,
+      });
+
+      const transactionUpdated = await this.transactionRepository.update({
+        id: transaction.id,
+        status: responsePagarme.status,
+        transactionId: String(responsePagarme.transactionId),
+        pixExpiration: responsePagarme.pix.expiration,
+        pixQRCODE: responsePagarme.pix.qrcode,
+        billetBarcode: responsePagarme.billet.barcode,
+        billetExpiration: responsePagarme.billet.expiration,
+        billetUrl: responsePagarme.billet.url,
+      });
+
+      return transactionUpdated;
+    } catch (err) {
+      return {
+        message: "Internal Server error",
+        statusCode: 400,
+      };
     }
-
-    const infosTransaction: CreateTransactionData = {
-      cartCode: String(cart.id),
-      code,
-      total: cart.price,
-      status: statusValid,
-      paymentType: paymentTypeValid,
-      installments,
-      customerName,
-      customerEmail,
-      customerDocument,
-      customerMobile: parsePhoneNumber(customerMobile, "BR").format("E.164"),
-      billingAddress,
-      billingCity,
-      billingNeighborhood,
-      billingNumber,
-      billingState,
-      billingZipCode,
-      billingComplementary,
-    };
-
-    const transaction = await this.transactionRepository.create(
-      infosTransaction
-    );
-
-    const creditCard = {
-      number: creditCardNumber,
-      holderName: creditCardHolderName,
-      expiration: creditCardExpiration,
-      cvv: creditCardCvv,
-    };
-    const customer = {
-      name: customerName,
-      email: customerEmail,
-      document: customerDocument,
-      mobile: parsePhoneNumber(customerMobile, "BR").format("E.164"),
-    };
-    const billing = {
-      name: "Billing Address",
-      state: billingState,
-      city: billingCity,
-      street: billingAddress,
-      neighborhood: billingNeighborhood,
-      number: billingNumber,
-      zipcode: billingZipCode,
-      complementary: billingComplementary,
-    };
-
-    const responsePagarme = await this.pagarMeProvider.process({
-      creditCard,
-      customer,
-      total: transaction.total,
-      billing,
-      installments,
-      transactionCode: transaction.code,
-      items: [],
-      paymentType,
-      pixExpirationDate,
-    });
-
-    const transactionUpdated = await this.transactionRepository.update({
-      id: transaction.id,
-      status: responsePagarme.status,
-      transactionId: String(responsePagarme.transactionId),
-      pixExpiration: responsePagarme.pix.expiration,
-      pixQRCODE: responsePagarme.pix.qrcode,
-      billetBarcode: responsePagarme.billet.barcode,
-      billetExpiration: responsePagarme.billet.expiration,
-      billetUrl: responsePagarme.billet.url,
-    });
-
-    return transactionUpdated;
   };
 }
