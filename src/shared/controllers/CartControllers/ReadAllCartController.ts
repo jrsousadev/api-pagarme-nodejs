f3d7306b8fcbeecdaec7e93c5ce2edecc10ec289
Junior Sousa <jrsousadev@gmail.com>
2022-10-17 20:04:57 -0300

    update module

--- src/shared/controllers/CartControllers/ReadAllCartController.ts
+++ src/shared/controllers/CartControllers/ReadAllCartController.ts
@@ -1,12 +1,15 @@
 import { Request, Response } from "express";
 import { container } from "tsyringe";
+import { AppError } from "../../errors/AppError";
 import { ReadAllCartUseCase } from "../../useCases/CartUseCases/ReadAllCartUseCase";
 
 class ReadAllCartController {
   async handle(request: Request, response: Response): Promise<any> {
     try {
       const readAllCartUseCase = container.resolve(ReadAllCartUseCase);
-      const carts = await readAllCartUseCase.execute();
+      const carts: any = await readAllCartUseCase.execute();
+
+      if (carts.message) throw new AppError(carts.message, carts.statusCode);
 
       return response.status(200).json(carts);
     } catch (err: any) {
